c4824ad4129cbe38551b71e2e3543f64b348fb06
Alok Singh <salok2857@gmail.com>
2025-11-23 16:31:10 +0530

    normal message send

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,11 @@ let userCount = 0;
 wss.on("connection", (socket) => {
   userCount = userCount + 1;
   console.log("counting users #" + userCount);
+
+  socket.on("message", (message) => {
+    console.log("message received: " + message);
+    setTimeout(() => {
+      socket.send(message.toString() + ":send from server");
+    }, 1000);
+  });
 });
